fbcc903609a59b765f8d3f246047e11636fc0944
Ajay Johar <ajayjohar3136@gmail.com>
2023-08-02 00:18:43 -0400

    TextForm Changes

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -17,13 +17,40 @@ export default function TextForm() {
         setText(textVal);
     }
 
+    const onClearClickHandler = ()=> {
+        let textVal = "";
+        setText(textVal);
+    }
+
+    const onCopyClickHandler = ()=> {
+        var textVal = document.getElementById("myText");
+        textVal.select();
+        navigator.clipboard.writeText(textVal.value);
+    }
+
+    const onPasteClickHandler = ()=> {
+        var textVal = document.getElementById("myText");
+        textVal.select();
+        navigator.clipboard.readText().then(cliptext =>
+            (setText(cliptext)));
+    }
+
+    const removeExtraSpaceHandler = ()=> {
+        var textVal = text.split(/[ ]+/);
+        setText(textVal.join(" "));
+    }
+
     return (
         <div>
             <div className="mb-3 container my-3">
                 <h2>Enter Your text here</h2>
                 <textarea className="form-control" value={text} onChange={onChangeHandler} id="myText" rows="8"></textarea>
-                <button className='btn btn-primary my-3 mx-3' onClick={onUpClickHandler}>Convert to UpperCase</button>
-                <button className='btn btn-primary my-3 mx-3' onClick={onLowerClickHandler}>Convert to lowerCase</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={onUpClickHandler}>Convert to UpperCase</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={onLowerClickHandler}>Convert to lowerCase</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={onClearClickHandler}>Clear</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={onCopyClickHandler}>Copy</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={onPasteClickHandler}>Paste</button>
+                <button className='btn btn-primary my-3 mx-2' onClick={removeExtraSpaceHandler}>Remove Space</button>
             </div>
             <div className='container mb-3'>
                 <h2>You text summary here</h2>
